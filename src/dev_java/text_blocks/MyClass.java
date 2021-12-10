c83a83149047c11874ea6bbca27076e6df4750e9
mxuexxmy <f9ab5014>
2021-12-10 23:15:47 +0800

    初始化

---
+++ src/dev_java/text_blocks/MyClass.java
@@ -0,0 +1,16 @@
+package dev_java.text_blocks;
+
+/**
+ * <h2> MyClass  </h2>
+ *
+ * @author mxuexxmy
+ * @date 2021/12/10 23:12
+ */
+public class MyClass {
+
+    double x;
+
+    public double getX() {
+        return x;
+    }
+}
---
+++ src/dev_java/text_blocks/test1.java
@@ -0,0 +1,14 @@
+package dev_java.text_blocks;
+
+/**
+ * <h2> test1  </h2>
+ *
+ * @author mxuexxmy
+ * @date 2021/12/10 23:11
+ */
+public class test1 {
+
+    public static void main(String[] args) {
+        var c1 = new MyClass();
+    }
+}
